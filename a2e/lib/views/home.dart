2760200f0bab248bf874c3593f0a168790bfdfbd
Rushil Rai <58342835+rushilrai@users.noreply.github.com>
2020-08-05 03:48:09 +0530

    UI Inhibited; Model Implemented;

--- a2e/lib/views/home.dart
+++ a2e/lib/views/home.dart
@@ -4,62 +4,89 @@ import 'package:a2e/views/viewfinder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:tflite/tflite.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
+String res;
+void loadModel() async {
+  res = await Tflite.loadModel(
+    model: "assets/model/phone_VGG16--88--08-03-03-29.tflite",
+    labels: "assets/model/labels_mobilenet_quant_v1_224.txt",
+  );
+}
+
 class _HomeScreenState extends State<HomeScreen> {
+  void testModel() async {
+    var recoginitions = await Tflite.runModelOnImage(
+      path: 'assets/model/testA.png',
+      imageMean: 255,
+      asynch: true,
+    );
+    print(recoginitions.toString());
+  }
+
+  @override
+  void initState() {
+    //loadModel();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: SystemUiOverlayStyle(
         statusBarIconBrightness: Brightness.dark,
-        statusBarColor: Theme.of(context).primaryColor,
+        statusBarColor: Theme.of(context).backgroundColor,
         systemNavigationBarColor: Theme.of(context).backgroundColor,
       ),
-      child: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title: Text(
-              'A2E',
-              style: TextStyle(
-                fontFamily: 'Comfortaa',
-                fontWeight: FontWeight.w800,
-                fontSize: displayWidth(context) * 0.06,
-                color: blackColor,
-              ),
-            ),
-          ),
-          backgroundColor: Theme.of(context).backgroundColor,
-          body: Container(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).backgroundColor,
+        body: SafeArea(
+          child: Container(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
+                Row(
+                  children: [
+                    Text(
+                      'A2E',
+                      style: TextStyle(
+                          fontFamily: 'Comfortaa',
+                          fontWeight: FontWeight.w800,
+                          fontSize: displayWidth(context) * 0.07),
+                    ),
+                  ],
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Container(
-                      height: displayWidth(context) * 0.13,
-                      width: displayWidth(context) * 0.3,
-                      child: RaisedButton(
-                        child: Icon(
-                          Icons.camera,
-                          color: Theme.of(context).textSelectionColor,
-                          size: displayWidth(context) * 0.1,
+                    Hero(
+                      tag: 'button',
+                      child: AnimatedContainer(
+                        duration: Duration(milliseconds: 200),
+                        height: displayWidth(context) * 0.13,
+                        width: displayWidth(context) * 0.25,
+                        child: RaisedButton(
+                          child: Icon(
+                            Icons.camera,
+                            color: Theme.of(context).textSelectionColor,
+                            size: displayWidth(context) * 0.1,
+                          ),
+                          elevation: 2,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          onPressed: () {
+                            Get.to(
+                              ViewfinderPage(),
+                            );
+                          },
+                          color: Theme.of(context).primaryColor,
                         ),
-                        elevation: 2,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        onPressed: () {
-                          Get.to(
-                            ViewfinderPage(),
-                          );
-                        },
-                        color: Theme.of(context).accentColor,
                       ),
                     ),
                   ],
--- a2e/lib/views/viewfinder.dart
+++ a2e/lib/views/viewfinder.dart
@@ -1,7 +1,9 @@
+import 'package:a2e/utils/colors.dart';
 import 'package:a2e/utils/sizes.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:tflite/tflite.dart';
 
 class ViewfinderPage extends StatefulWidget {
   @override
@@ -9,23 +11,61 @@ class ViewfinderPage extends StatefulWidget {
 }
 
 class _ViewfinderPageState extends State<ViewfinderPage> {
+  void testModel() async {
+    var recoginitions = await Tflite.runModelOnImage(
+      path: 'assets/model/testA.png',
+      imageMean: 255,
+      asynch: true,
+    );
+    print(recoginitions.toString());
+  }
+
   CameraController _controller;
   Future<void> _initializeControllerFuture;
   bool isCameraReady = false;
 
   Future<void> _initializeCamera() async {
     final cameras = await availableCameras();
-    final firstCamera = cameras.first;
+    final firstCamera = cameras[0];
     _controller = CameraController(firstCamera, ResolutionPreset.high);
-    _initializeControllerFuture = _controller.initialize();
-    if (!mounted) {
-      return;
-    }
-    setState(() {
-      isCameraReady = true;
+    _initializeControllerFuture = _controller.initialize().then((_) async {
+      if (!mounted) {
+        return;
+      }
+      setState(() {
+        isCameraReady = true;
+      });
+      String res = await Tflite.loadModel(
+        model: "assets/model/phone_VGG16--88--08-03-03-29.tflite",
+        labels: "assets/model/labels.txt",
+      );
+      print('start');
+      _controller.startImageStream(
+        (image) async {
+          Tflite.runModelOnFrame(
+            bytesList: image.planes.map((plane) {
+              return plane.bytes;
+            }).toList(),
+            imageHeight: image.height,
+            imageWidth: image.width,
+            threshold: 0.5,
+            numResults: 1,
+            asynch: true,
+          ).then((value) {
+            value.map((res) {});
+            print(value.first);
+            //   print('yes');
+            setState(() {
+              label = value.first['label'].toString();
+            });
+            //   print(label);
+          });
+        },
+      );
     });
   }
 
+  String label = '';
   @override
   void initState() {
     _initializeCamera();
@@ -40,32 +80,50 @@ class _ViewfinderPageState extends State<ViewfinderPage> {
 
   @override
   Widget build(BuildContext context) {
-    if (isCameraReady) {
-      return AnnotatedRegion<SystemUiOverlayStyle>(
-        value: SystemUiOverlayStyle(
-          statusBarColor: Theme.of(context).primaryColor,
-          statusBarIconBrightness: Brightness.dark,
-        ),
-        child: SafeArea(
-          child: Scaffold(
-            backgroundColor: Theme.of(context).backgroundColor,
-            body: Column(
-              children: [
-                AspectRatio(
-                  aspectRatio: 2 / 3,
-                  child: CameraPreview(_controller),
-                ),
-                Expanded(
-                  child: Container(
-                    color: Theme.of(context).backgroundColor,
-                    width: displayWidth(context),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+      child: Scaffold(
+          backgroundColor: Theme.of(context).backgroundColor,
+          body: (isCameraReady)
+              ? Column(
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 2 / 3,
+                      child: (isCameraReady)
+                          ? CameraPreview(_controller)
+                          : Container(),
+                    ),
+                    Expanded(
+                      child: Hero(
+                        tag: 'button',
+                        child: AnimatedContainer(
+                          duration: Duration(milliseconds: 200),
+                          color: Theme.of(context).backgroundColor,
+                          width: displayWidth(context),
+                          child: Center(
+                            child: Text(
+                              label,
+                              style: TextStyle(
+                                color: Theme.of(context).accentColor,
+                                fontFamily: 'Comfortaa',
+                                fontWeight: FontWeight.w800,
+                                fontSize: displayWidth(context) * 0.14,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              : Container(
+                  child: Center(
+                    child: CircularProgressIndicator(),
                   ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      );
-    }
+                )),
+    );
   }
 }
